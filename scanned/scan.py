4ec85599351d7990cbd5fbd26259d457f45db709
Suriya Parithy <95739525+parithysuriya@users.noreply.github.com>
2022-10-29 11:47:29 +0530

    Add files via upload

---
+++ scanned/scan.py
@@ -0,0 +1,51 @@
+# Import libraries
+from PIL import Image
+from pytesseract import pytesseract
+import sys
+from pdf2image import convert_from_path
+import os
+
+filepdf = 'document.pdf'
+
+#Define path to tessaract.exe
+path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+#Point tessaract_cmd to tessaract.exe
+pytesseract.tesseract_cmd = path_to_tesseract
+
+if os.path.exists(filepdf):
+    # Increase DPI (300) if image quality is too poor
+    images = convert_from_path(filepdf, 300)
+    # i counts each page
+    i = 1
+
+    # Process each page as image
+    for image in images:
+        filename = "pdfimage"+str(i)+".jpg"
+        image.save(filename, 'JPEG')
+        i = i + 1
+
+    # Output file name, should be the same as pdf but with txt extension.
+    outfile = "out_text.txt"
+
+    # Open the target file
+    f = open(outfile, "a")
+
+    # Iterate from 1 to total number of pages
+    # Read each img text and append it to document
+    for i in range(1, i):
+        readimage = Image.open("pdfimage"+str(i)+".jpg")
+        text = str(((pytesseract.image_to_string(readimage))))
+        f.write(text)
+        # remove the processed image
+        if os.path.exists("pdfimage"+str(i)+".jpg"):
+            os.remove("pdfimage"+str(i)+".jpg")
+
+    # Close file
+    f.close()
+
+    print(outfile)
+else:
+    print("Argument is not a valid filename")
+    
+# Reference - https://github.com/carcagi/pdf-text-extract/blob/main/pdf-text-extract.py
